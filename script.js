ba473e98c03aa2098bc101dfdbe823b585feea75
prog-ammar <ammarofc2005@gmail.com>
2024-11-24 13:31:19 +0500

    Update script.js
    updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('download-btn').addEventListener('click', () => {
-    const fileUrl = 'antivirus-software.zip'; // Replace with the actual file path
+    const fileUrl = 'Antivirus.exe'; // Replace with the actual file path
     const anchor = document.createElement('a');
     anchor.href = fileUrl;
-    anchor.download = 'SecureGuardAntivirus.zip'; // The file name for download
+    anchor.download = 'Antivirus.exe'; // The file name for download
     document.body.appendChild(anchor);
     anchor.click();
     document.body.removeChild(anchor);
